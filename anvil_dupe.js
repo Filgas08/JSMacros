b362142456e13e7ea4752217c9bd5f3f0a4865c3
Filgas08 <47673040+Filgas08@users.noreply.github.com>
2023-07-26 18:50:09 +0200

    Update anvil_dupe.js

--- anvil_dupe.js
+++ anvil_dupe.js
@@ -5,94 +5,123 @@ var wait = 4; ///If it doesen't work, keep adding 1 number until it works.
 const loop_count = 20; ///set how many times you want it to repeat
 var anvil = true //true to automaticaly place anvils once they run out false to not
 
-for (let i = 0; i < loop_count; ++i) {
-    Player.getPlayer().lookAt(0, 90);
-    try {
-        var Data = Player.rayTraceBlock(5, false).getId();
-    } catch {}
-    if (Data == "minecraft:anvil" || Data == "minecraft:chipped_anvil" || Data == "minecraft:damaged_anvil") {
-        Player.openInventory().setSelectedHotbarSlotIndex(0);
-        Client.waitTick(+wait)
-        Player.getPlayer().interact();
-        Client.waitTick(+wait);
-        Player.openInventory().swap(30, 0);
-        Client.waitTick(+wait);
-        if (Player.openInventory().getName() == "X2 Beacon") {
-            Player.openInventory().seName("By X2 Beacon");
-        } else if (Player.openInventory().getName() == "By X2 Beacon") {
-            Player.openInventory().seName("X2 Beacon");
-        }
-        Client.waitTick(+wait);
-        Player.openInventory().swap(2, 30);
-        Client.waitTick(+wait);
-        Player.openInventory().close();
-        ///drop duped item
-        Client.waitTick(+wait);
-        Player.getPlayer().lookAt(0, 50);
-        Client.waitTick(+wait);
-        Player.openInventory().swap(37, 36);
-        Client.waitTick(+wait);
-        Player.openInventory().dropSlot(36);
-        Client.waitTick(+wait);
-        Player.getPlayer().lookAt(0, 90);
-        Client.waitTick(+wait);
-        Player.openInventory().swap(36, 37);
-        ///
-        Player.openInventory().setSelectedHotbarSlotIndex(8);
-        Player.getPlayer().lookAt(0, -90);
-        Client.waitTick(+wait);
-        for (let i = 0; i < 6; i++) {
-            Player.getPlayer().interactItem(false);
-            Client.waitTick(5);
-        }
-        Client.waitTick(+wait);
-        Player.getPlayer().lookAt(0, 90);
-        Client.waitTick(+wait);
+main()
 
-        // exp refill
-        if (Player.openInventory().getSlot(44).getItemId() != "minecraft:experience_bottle") {
-            for (let i = 9; i < 35; i++) {
-                if (Player.openInventory().getSlot(i).getItemId() == "minecraft:experience_bottle") {
-                    Player.openInventory().swap(i, 44);
-                }
+function main() {
+    for (let i = 0; i < loop_count; ++i) {
+        Player.getPlayer().lookAt(0, 90);
+        try {
+            var Data = Player.rayTraceBlock(5, false).getId();
+        } catch {}
+        if (Data == "minecraft:anvil" || Data == "minecraft:chipped_anvil" || Data == "minecraft:damaged_anvil") {
+            Player.openInventory().setSelectedHotbarSlotIndex(0);
+            Client.waitTick(+wait)
+            Player.getPlayer().interact();
+            Client.waitTick(+wait);
+            Player.openInventory().swap(30, 0);
+            Client.waitTick(+wait);
+            if (Player.openInventory().getName() == "X2 Beacon") {
+                Player.openInventory().seName("By X2 Beacon");
+            } else if (Player.openInventory().getName() != "X2 Beacon") {
+                Player.openInventory().seName("X2 Beacon");
             }
+            Client.waitTick(+wait);
+            Player.openInventory().swap(2, 30);
+            Client.waitTick(+wait);
+            Player.openInventory().close();
+            ///drop duped item
+            Client.waitTick(+wait);
+            Player.getPlayer().lookAt(0, 50);
+            Client.waitTick(+wait);
+            Player.openInventory().swap(37, 36);
+            Client.waitTick(+wait);
+            Player.openInventory().dropSlot(36);
+            Client.waitTick(+wait);
+            Player.getPlayer().lookAt(0, 90);
+            Client.waitTick(+wait);
+            Player.openInventory().swap(36, 37);
+            ///
+            Player.openInventory().setSelectedHotbarSlotIndex(8);
+            Player.getPlayer().lookAt(0, -90);
+            Client.waitTick(+wait);
+            if (exp() == 1) {
+                return 0;
+            }
+            Client.waitTick(+wait);
+            Player.getPlayer().lookAt(0, 90);
+            Client.waitTick(+wait);
+        } else if (i != 0) {
+            pillar();
+        } else {
+            Chat.log("stand on top of an anvil to use this script!")
+            return 0;
         }
-        // notify when XP runs out
-        if (Player.openInventory().findItem("minecraft:experience_bottle").length == 0) {
-            World.playSound("block.note_block.cow_bell");
-            Chat.log("out of xp");
-            quit();
-        }
-    } else if (i != 0) {
-        pillar();
-    } else {
-        Chat.log("stand on top of an anvil to use this script!")
-        quit();
     }
+}
+
+function exp() {
+    var missing = 0;
+    for (let i = 0; i < 3; i++) {
+        exp = Player.getPlayer().getXPProgress();
+        Chat.log(+exp);
+        missing = 7 - exp;
+        if (missing <= 0) {
+            return 0;
+        }
+        if (check_and_refill() == 1) {
+            return 1;
+        }
+        Player.getPlayer().interactItem(false);
+        Client.waitTick(5);
+    }
+    return 0
 }
 
 function pillar() {
     ///pillar up
-    if(anvil == true){
-        const loop_count = 16;
-        for (let i = 0; i < loop_count; ++i) {
-            const inv = Player.openInventory(); // Class to allow selection of slot
-            inv.setSelectedHotbarSlotIndex(5); // Select hotbar slot with block to place
-            Client.waitTick(1); // Delay 1ms
-            const inp = Player.createPlayerInput(0, 0, 0, 90, true, true, false); // Sneak & Jump & look down
-            const inp1 = Player.createPlayerInput(0, 0, 0, 90, false, true, false); // Sneak & look down
-            Player.addInput(inp); // Execute movement from line above
-            Client.waitTick(1)
-            //Player.getPlayer().lookAt(0,90) // Look down
-            Client.waitTick(1);
-            Player.addInput(inp1); //|| Player.addInput(inp1)
-            Client.waitTick(1);
-            Player.getPlayer().interact(); // Place block down
-            Client.waitTick(5);
+    if (Player.openInventory().getSlot(44).getItemId() == "minecraft:anvil" ||
+        Player.openInventory().getSlot(44).getItemId() == "minecraft:chipped_anvil" ||
+        Player.openInventory().getSlot(44).getItemId() == "minecraft:damaged_anvil") {
+        if (anvil == true) {
+            const loop_count = 16;
+            for (let i = 0; i < loop_count; ++i) {
+                const inv = Player.openInventory(); // Class to allow selection of slot
+                inv.setSelectedHotbarSlotIndex(5); // Select hotbar slot with block to place
+                Client.waitTick(1); // Delay 1ms
+                const inp = Player.createPlayerInput(0, 0, 0, 90, true, true, false); // Sneak & Jump & look down
+                const inp1 = Player.createPlayerInput(0, 0, 0, 90, false, true, false); // Sneak & look down
+                Player.addInput(inp); // Execute movement from line above
+                Client.waitTick(1)
+                //Player.getPlayer().lookAt(0,90) // Look down
+                Client.waitTick(1);
+                Player.addInput(inp1); //|| Player.addInput(inp1)
+                Client.waitTick(1);
+                Player.getPlayer().interact(); // Place block down
+                Client.waitTick(5);
+            }
         }
     }
 }
 
 function quit() {
     JsMacros.getServiceManager().stopService("anvil_dupe.js");
+}
+
+function check_and_refill() {
+    // exp refill
+    if (Player.openInventory().getSlot(44).getItemId() != "minecraft:experience_bottle") {
+        for (let i = 9; i < 35; i++) {
+            if (Player.openInventory().getSlot(i).getItemId() == "minecraft:experience_bottle") {
+                Player.openInventory().swap(i, 44);
+                return 0;
+            }
+        }
+    }
+    // notify when XP runs out
+    if (Player.openInventory().findItem("minecraft:experience_bottle").length == 0) {
+        World.playSound("block.note_block.cow_bell");
+        Chat.log("out of xp");
+        return 1;
+    }
+    return 0;
 }
